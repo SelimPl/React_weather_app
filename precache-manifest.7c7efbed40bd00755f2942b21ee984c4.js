dfd51a5e49d3ee68bff9e695e30b68238e1842f1
Selim Pl <selosdz@gmail.com>
2020-02-02 19:14:44 +0100

    Updates

---
+++ precache-manifest.7c7efbed40bd00755f2942b21ee984c4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "02bd18ae923055ebcbe2529626a87935",
+    "url": "/React_weather_app/index.html"
+  },
+  {
+    "revision": "f9dda05957bf9708e5a1",
+    "url": "/React_weather_app/static/css/main.e9319632.chunk.css"
+  },
+  {
+    "revision": "e2a36e4b8723ce38ce66",
+    "url": "/React_weather_app/static/js/2.311c3e75.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/React_weather_app/static/js/2.311c3e75.chunk.js.LICENSE"
+  },
+  {
+    "revision": "f9dda05957bf9708e5a1",
+    "url": "/React_weather_app/static/js/main.f5ff9f90.chunk.js"
+  },
+  {
+    "revision": "aeba9ecda965f3ef9895",
+    "url": "/React_weather_app/static/js/runtime-main.3c9f4ead.js"
+  }
+]);
